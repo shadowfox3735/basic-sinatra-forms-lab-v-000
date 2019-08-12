f838cc05b471be6a01799b021736a7cdfccbfca4
shadowfox3735 <normanpan180@gmail.com>
2019-08-12 20:28:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,8 @@ class App < Sinatra::Base
     @small_forward = params[:small_forward]
     @power_forward = params[:power_forward]
     @center = params[:center]
+    
+    erb :team
   end
   
 end
